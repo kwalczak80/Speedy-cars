7380837730c0cd18055af9c81d9af335dd21c210
kwalczak80 <christopher.walczak@gmail.com>
2022-08-10 12:06:54 +0000

    Update base.html code with jinja template

--- home/views.py
+++ home/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello world!")
+    return render(request, 'pages/index.html')
+
+def about(request):
+    return render(request, 'pages/about.html')
