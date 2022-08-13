7570e1db4294051f18a9b5491d349764a0700221
kwalczak80 <christopher.walczak@gmail.com>
2022-08-13 16:05:40 +0000

    Update car model

--- cars/models.py
+++ cars/models.py
@@ -3,14 +3,21 @@ from cloudinary.models import CloudinaryField
 import datetime
 
 class Car(models.Model):
-    MAKE = (
+    title = models.CharField(max_length=300, null=False, blank=False)
+    MAKE = [
         ('Audi', 'Audi'),
         ('Bmw', 'Bmw'),
+        ('Citroën', 'Citroën'),
         ('Ford', 'Ford'),
         ('Mazda', 'Mazda'),
+        ('Skoda', 'Skoda'),
         ('Toyota', 'Toyota'),
+        ('Opel', 'Opel'),
         ('Lexus', 'Lexus'),
-    )
+        ('Mercedes', 'Mercedes'),
+        ('Volkswagen', 'Volksvagen'),
+        ('Volvo', 'Volvo'),
+    ]
     make = models.CharField(max_length=50, choices=MAKE)
     model = models.CharField(max_length=50)
     price = models.IntegerField()
@@ -21,6 +28,7 @@ class Car(models.Model):
         ('Electric', 'Electric'),
     )
     fuel_type = models.CharField(max_length=50, choices=FUEL_TYPE)
+    engine_size = models.FloatField()
     YEAR_CHOICES = [(r, r) for r in range(2005, datetime.date.today().year+1)]
     year = models.IntegerField(('year'), choices=YEAR_CHOICES, default=datetime.datetime.now().year)
     BODY = (
@@ -28,6 +36,7 @@ class Car(models.Model):
         ('Hatchback', 'Hatchback'),
         ('Coupe', 'Coupe'),
         ('Estate', 'Estate'),
+        ('SUV', 'SUV'),
 
     )
     body = models.CharField(max_length=50, choices=BODY)
@@ -36,9 +45,14 @@ class Car(models.Model):
         ('Automatic', 'Automatic'),
     )
     transmmission = models.CharField(max_length=50, choices=TRANSMISSION)
+    colour = models.CharField(max_length=50)
+    owners = models.IntegerField()
+    milage = models.FloatField()
     description = models.TextField()
     date = models.DateField(auto_now_add=True)
     picture = CloudinaryField('image')
 
     def __str__(self):
-        return self.make.choices
+        return self.title
+
+    
