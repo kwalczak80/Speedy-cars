fc78e68eb99cd5e84b52bed10b164b1ddd7431d5
kwalczak80 <christopher.walczak@gmail.com>
2022-08-14 20:16:44 +0000

    Add customized admin panel for emplyees

--- employees/admin.py
+++ employees/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
 from .models import Employee
 
-admin.site.register(Employee)
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'email', 'hire_date')
+    list_display_links = ('id', 'name')
+    search_fields = ('name',)
+
+
+admin.site.register(Employee, EmployeeAdmin)
