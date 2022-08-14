24c20caa1c70d9ad3454f4d47211c27e94e85871
kwalczak80 <christopher.walczak@gmail.com>
2022-08-14 19:53:25 +0000

    Add option to choose if car is for sale or not in admin panel

--- cars/admin.py
+++ cars/admin.py
@@ -4,6 +4,7 @@ from .models import Car
 class CarAdmin(admin.ModelAdmin):
     list_display = ('id', 'title', 'is_for_sale', 'price', 'date')
     list_display_links = ('id', 'title')
+    list_editable = ('is_for_sale',)
 
 
 admin.site.register(Car, CarAdmin)
