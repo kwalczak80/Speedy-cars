68d6afbe2ed3167b3060ddfb1616fdd4d0543942
kwalczak80 <christopher.walczak@gmail.com>
2022-08-18 20:45:48 +0000

    Import Paginator module and update index class to initiate pagination

--- cars/views.py
+++ cars/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 
 from .models import Car
 
 def index(request):
     cars = Car.objects.all()
+    paginator = Paginator(cars, 6)
+    page = request.GET.get('page')
+    paged_cars = paginator.get_page(page)
 
     context = {
-        'cars': cars
+        'cars': paged_cars
     }
     return render(request, 'cars/cars.html', context)
 
