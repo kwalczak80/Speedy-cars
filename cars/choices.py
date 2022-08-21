3f26f1e3ed475d4520010086df6e5abab094ec09
kwalczak80 <christopher.walczak@gmail.com>
2022-08-21 13:44:05 +0000

    Create choices file which will be used on the search page

---
+++ cars/choices.py
@@ -0,0 +1,61 @@
+make_choices = {
+    'Audi': 'Audi',
+    'Bmw': 'Bmw',
+    'Citroen': 'Citroen',
+    'Ford': 'Ford',
+    'Mazda': 'Mazda',
+    'Skoda': 'Skoda',
+    'Toyota': 'Toyota',
+    'Opel': 'Opel',
+    'Lexus': 'Lexus',
+    'Mercedes': 'Mercedes',
+    'Volksvagen': 'Volksvagen',
+    'Volvo': 'Volvo',
+}
+
+fuel_type_choices = {
+    'Petrol': 'Petrol',
+    'Diesel': 'Diesel',
+    'Hybrid': 'Hybrid',
+    'Electric': 'Electric',
+}
+
+body_type_choices = {
+    'Coupe': 'Coupe',
+    'Saloon': 'Saloon',
+    'Hatchback': 'Hatchback',
+    'Estate': 'Estate',
+    'SUV': 'SUV',
+}
+
+engine_size_choices = {
+    '1.0L': '1.0L',
+    '1.1L': '1.1L',
+    '1.2L': '1.2L',
+    '1.3L': '1.3L',
+    '1.4L': '1.4L',
+    '1.5L': '1.5L',
+    '1.6L': '1.6L',
+    '1.7L': '1.7L',
+    '1.8L': '1.8L',
+    '1.9L': '1.9L',
+    '2.0L': '2.0L',
+    '2.5L': '2.5L',
+    '3.0L': '3.0L',
+    '3.5L': '3.5L',
+    '4.0L': '4.0L',
+    '5.0L': '5.0L',
+}
+
+price_choices = {
+    '5000': '€5000',
+    '10000': '€10000',
+    '15000': '€15000',
+    '20000': '€20000',
+    '25000': '€25000',
+    '30000': '€30000',
+    '35000': '€35000',
+    '40000': '€40000',
+    '45000': '€45000',
+    '50000': '€50000+',
+}
