a6a6e136bc2b3166e21d247988f731ecf4b9e8de
kwalczak80 <christopher.walczak@gmail.com>
2022-08-21 13:46:21 +0000

    Update urlpatterns with search_results

--- cars/urls.py
+++ cars/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='cars'),
     path('<int:car_id>', views.car, name='car'),
-    path('search', views.search, name='search'),
+    path('search_results', views.search_results, name='search_results'),
 ]
