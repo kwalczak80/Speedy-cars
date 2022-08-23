84ce5e32ba268905fb9903c90b2618a1f3eb3103
kwalczak80 <christopher.walczak@gmail.com>
2022-08-23 13:56:34 +0000

    Change booking model structure

--- booking/models.py
+++ booking/models.py
@@ -2,13 +2,10 @@ from django.db import models
 
 class Booking(models.Model):
     car = models.CharField(max_length=100)
-    car_id = models.IntegerField()
     name = models.CharField(max_length=200)
     email = models.CharField(max_length=100)
     phone = models.CharField(max_length=100)
-    date = models.DateField()
-    time = models.TimeField()
-    user_id = models.IntegerField(blank=True)
+    message = models.TextField(blank=True)
     
     def __str__(self):
         return str(self.name)
