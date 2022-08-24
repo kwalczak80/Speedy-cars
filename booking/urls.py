7de9ebc8c4a838131edf48b7b8b8daf6284d4c6e
kwalczak80 <christopher.walczak@gmail.com>
2022-08-24 13:49:02 +0000

    Update urlpatterns in booking app

--- booking/urls.py
+++ booking/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('booking', views.booking, name='booking')
+    path('booking', views.booking, name='booking'),
+    path('dashboard', views.dashboard, name='dashboard')
 ]
