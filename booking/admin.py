0cdeb0bd7f483a2a42862dc391ab61af4db4f21f
kwalczak80 <christopher.walczak@gmail.com>
2022-08-26 10:22:53 +0000

    Update car model with car_id. Update admin panel to display car_id

--- booking/admin.py
+++ booking/admin.py
@@ -3,10 +3,9 @@ from django.contrib import admin
 from .models import Booking
 
 class BookingAdmin(admin.ModelAdmin):
-    list_display = ('id', 'user_id', 'name', 'car', 'email')
+    list_display = ('id', 'user_id', 'name', 'car', 'car_id', 'email')
     list_display_links = ('id', 'name')
     search_fields = ('name', 'email', 'car')
     list_per_page = 15
 
 admin.site.register(Booking, BookingAdmin)
-
--- booking/models.py
+++ booking/models.py
@@ -4,12 +4,14 @@ import datetime
 
 class Booking(models.Model):
     car = models.CharField(max_length=100)
+    car_id = models.IntegerField(default=0)
     name = models.CharField(max_length=200)
     email = models.CharField(max_length=100)
     phone = models.CharField(max_length=100)
     date = models.DateField(default=datetime.date.today)
     message = models.TextField(blank=True)
     user_id = models.IntegerField(default=0)
+    
     
     def __str__(self):
         return str(self.name)
