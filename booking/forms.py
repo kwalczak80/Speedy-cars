73d2787e1a005e37555a3dce002744e43b2f730a
kwalczak80 <christopher.walczak@gmail.com>
2022-08-29 15:28:51 +0000

    Create BookingForm class in forms.py file

---
+++ booking/forms.py
@@ -0,0 +1,19 @@
+from django import forms
+from .models import Booking
+
+class DateInput(forms.DateInput):
+    input_type = 'date'
+
+class BookingForm(forms.ModelForm):
+    class Meta:
+        model = Booking
+
+        fields = ['car', 'name', 'email', 'phone', 'date', 'message']
+        widgets = {
+            'car': forms.TextInput(attrs={'class': 'form-control'}),
+            'name': forms.TextInput(attrs={'class': 'form-control'}),
+            'email': forms.TextInput(attrs={'class': 'form-control'}),
+            'phone': forms.TextInput(attrs={'class': 'form-control'}),
+            'date': forms.DateInput(attrs={'class': 'form-control'}),
+            'message': forms.Textarea(attrs={'class': 'form-control'}),
+        }
