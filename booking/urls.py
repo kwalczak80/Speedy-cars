ef44f3985d79dac6c3d15a7dd6033ea8c51b46b9
kwalczak80 <christopher.walczak@gmail.com>
2022-09-05 11:18:03 +0000

    Update booking url patterns

--- booking/urls.py
+++ booking/urls.py
@@ -5,6 +5,6 @@ from . import views
 urlpatterns = [
     path('booking', views.booking, name='booking'),
     path('dashboard', views.dashboard, name='dashboard'),
-    path('<booking_id>', views.cancellation, name='cancellation'),
+    path('cancellation/<booking_id>/', views.cancellation, name='cancellation'),
     path('edit/<int:booking_id>/', views.edit, name='edit'),
 ]
