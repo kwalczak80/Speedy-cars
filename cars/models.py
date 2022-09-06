94f87cc27cb6d445e19bf5bfc798f1d52c223b17
kwalczak80 <christopher.walczak@gmail.com>
2022-09-06 10:06:00 +0000

    Update cars models after pep8 check

--- cars/models.py
+++ cars/models.py
@@ -3,9 +3,12 @@ from django.db import models
 from cloudinary.models import CloudinaryField
 from employees.models import Employee
 
+
 class Car(models.Model):
-    sales_person = models.ForeignKey(Employee, on_delete=models.DO_NOTHING, default=True)
-    title = models.CharField(max_length=300, null=False, blank=False, default='')
+    sales_person = models.ForeignKey(Employee, on_delete=models.DO_NOTHING,
+                                     default=True)
+    title = models.CharField(max_length=300, null=False, blank=False,
+                             default='')
     MAKE = (
         ('Audi', 'Audi'),
         ('Bmw', 'Bmw'),
@@ -29,12 +32,14 @@ class Car(models.Model):
         ('Hybrid', 'Hybrid'),
         ('Electric', 'Electric')
     )
-    fuel_type = models.CharField(max_length=50, choices=FUEL_TYPE, default=FUEL_TYPE[0][0])
+    fuel_type = models.CharField(max_length=50, choices=FUEL_TYPE,
+                                 default=FUEL_TYPE[0][0])
     TRANSMISSION = (
         ('Manual', 'Manual'),
         ('Automatic', 'Automatic')
     )
-    transmission = models.CharField(max_length=50, choices=TRANSMISSION, default=TRANSMISSION[0][0])
+    transmission = models.CharField(max_length=50, choices=TRANSMISSION,
+                                    default=TRANSMISSION[0][0])
     BODY_TYPE = (
         ('Coupe', 'Coupe'),
         ('Saloon', 'Saloon'),
@@ -42,9 +47,11 @@ class Car(models.Model):
         ('Estate', 'Estate'),
         ('SUV', 'SUV')
     )
-    body_type = models.CharField(max_length=50, choices=BODY_TYPE, default=BODY_TYPE[0][0])
+    body_type = models.CharField(max_length=50, choices=BODY_TYPE,
+                                 default=BODY_TYPE[0][0])
     YEAR_CHOICES = [(r, r) for r in range(2010, datetime.date.today().year+1)]
-    year = models.IntegerField(('year'), choices=YEAR_CHOICES, default=datetime.datetime.now().year)
+    year = models.IntegerField(('year'), choices=YEAR_CHOICES,
+                               default=datetime.datetime.now().year)
     ENGINE_SIZE = (
         ('1.0L', '1.0L'),
         ('1.1L', '1.1L'),
@@ -63,14 +70,15 @@ class Car(models.Model):
         ('4.0L', '4.0L'),
         ('5.0L', '5.0L'),
     )
-    engine_size = models.CharField(max_length=20, choices=ENGINE_SIZE, default=ENGINE_SIZE[0][0])
+    engine_size = models.CharField(max_length=20, choices=ENGINE_SIZE,
+                                   default=ENGINE_SIZE[0][0])
     milage = models.IntegerField(default=1)
     description = models.TextField(default='')
     previous_owners = models.IntegerField(default=1)
     road_tax = models.IntegerField(default=1)
     nct = models.DateField(auto_now=True)
     is_for_sale = models.BooleanField(default=True)
-    photo_main = CloudinaryField('image') 
+    photo_main = CloudinaryField('image')
     photo_1 = CloudinaryField('image', blank=True)
     photo_2 = CloudinaryField('image', blank=True)
     photo_3 = CloudinaryField('image', blank=True)
@@ -81,5 +89,3 @@ class Car(models.Model):
 
     def __str__(self):
         return self.title
-
-    
