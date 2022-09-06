ee573503ea3217cd9981e22c8e5b35d60ed597e3
kwalczak80 <christopher.walczak@gmail.com>
2022-09-06 11:06:45 +0000

    Update booking  model after pep8 check

--- booking/models.py
+++ booking/models.py
@@ -1,6 +1,6 @@
 from django.db import models
-from django.contrib.auth.models import User
 import datetime
+
 
 class Booking(models.Model):
     car = models.CharField(max_length=100)
@@ -11,7 +11,7 @@ class Booking(models.Model):
     date = models.DateField(default=datetime.date.today)
     message = models.TextField(blank=True)
     user_id = models.IntegerField(default=0)
-    
-    
+
     def __str__(self):
         return str(self.name)
+
