5e488d800da8a50b5131e4121e84a78ad12b7dd6
kwalczak80 <christopher.walczak@gmail.com>
2022-09-14 20:06:38 +0000

    Create script.js file with java script code. Add link in the base.html file

---
+++ static/js/script.js
@@ -0,0 +1,17 @@
+// Timer showing the allert messages
+setTimeout(function () {
+    let messages = document.getElementById('msg');
+    let alert = new bootstrap.Alert(messages);
+    alert.close();
+}, 5000);
+
+// Apply datepicker styling for test drive booking form
+$('#datepicker').datepicker({
+    format: 'dd/mm/yyyy'
+});
+$('#datepicker').datepicker("setDate", new Date());
+
+// Highlight current day of the week
+$(document).ready(function () {
+    $('.opening-hours li').eq(new Date().getDay()).addClass('today');
+});
